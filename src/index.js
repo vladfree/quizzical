6aae11b1ab4b5d5e895c849024dd481b6921e95b
“vladfree” <vladc.freelancer@gmail.com>
2022-07-20 23:58:44 +0300

    fixed double rendering

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 
 );
 
