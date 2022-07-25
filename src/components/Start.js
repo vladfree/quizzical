3eccd3016a798d456351ca182e52d17eefdbc1e1
“vladfree” <vladc.freelancer@gmail.com>
2022-07-25 22:42:24 +0300

    cleanup

--- src/components/Start.js
+++ src/components/Start.js
@@ -6,11 +6,11 @@ export default function Start (props){
     return (
         <div className="box">
             <div className="inner">
-                <img className="img-top" src={require("../images/topRight.png")} alt="" />
+                <img className="img-top" src={require("../images/startTop.png")} alt="" />
                 <h1>Quizzical</h1>
                 <p>Test your knowladge with a large variety of Trivia questions!</p>
                 <Link to="/questions"> <button className="start-btn">Start quiz</button> </Link>
-                <img className="img-bottom" src={require("../images/bottomLeft.png")} alt="" />
+                <img className="img-bottom" src={require("../images/startBottom.png")} alt="" />
             </div>
         </div>
     )
